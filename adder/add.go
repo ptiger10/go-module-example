2b0e2713d6e0869c44dcbcb019ec0e892d8a2926
Dave Fort <dhfort@gmail.com>
2019-03-07 15:06:38 -0800

    clean up doc

--- adder/add.go
+++ adder/add.go
@@ -1,9 +1,6 @@
 package adder
 
 // Add an arbitrary collection of ints
-// Return an int
-// Not that interesting
-// Another try
 func Add(nums ...int) int {
 	var answer int
 	for _, num := range nums {
